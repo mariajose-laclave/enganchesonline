4581097c85bffc773fc4251185f3cb539c22d3ce
Grant MacDonald <phisoluciones.es@gmail.com>
2021-03-29 08:53:14 +0200

    Test scripts

---
+++ scripts/create-products.php
@@ -0,0 +1,44 @@
+<?php
+require dirname(__FILE__) . '/../app/bootstrap.php';
+$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+require dirname(__FILE__) . '/abstract.php';
+
+class CreateCategoriesApp extends AbstractApp
+{
+
+    public function run()
+    {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $product = $objectManager->create('\Magento\Catalog\Model\Product');
+        $product->setSku('my-sku'); // Set your sku here
+        $product->setName('Sample Simple Product'); // Name of Product
+        $product->setAttributeSetId(4); // Attribute set id
+        $product->setStatus(1); // Status on product enabled/ disabled 1/0
+        $product->setWeight(10); // weight of product
+        $product->setVisibility(4); // visibilty of product (catalog / search / catalog, search / Not visible individually)
+        $product->setTaxClassId(0); // Tax class id
+        $product->setTypeId('simple'); // type of product (simple/virtual/downloadable/configurable)
+        $product->setPrice(100); // price of product
+        $product->setStockData(
+            array(
+                'use_config_manage_stock' => 0,
+                'manage_stock' => 1,
+                'is_in_stock' => 1,
+                'qty' => 999999999
+            )
+        );
+        $product->save();
+        /*
+        $this->_objectManager->get('Magento\Framework\Registry')
+            ->register('isSecureArea', true);
+
+        $category = $this->_objectManager->create('\Magento\Catalog\Model\Category');
+        $category = $category->load(343);
+
+        $category->delete();*/
+    }
+}
+
+/** @var \Magento\Framework\App\Http $app */
+$app = $bootstrap->createApplication('CreateCategoriesApp');
+$bootstrap->run($app);
