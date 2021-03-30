176caf1d976d52009633066a7d0bbae26a9f90d7
Grant MacDonald <phisoluciones.es@gmail.com>
2021-03-30 15:12:34 +0200

    Convert uniqid to string

--- scripts/create-products.php
+++ scripts/create-products.php
@@ -99,7 +99,7 @@ class CreateCategoriesApp extends AbstractApp
             try {
                 $objectManager->get('\Magento\Catalog\Api\CategoryRepositoryInterface')->save($category);
             } catch (Exception $e) {
-                $category->setUrl($category->getUrl() + (string)uniqid());
+                $category->setUrl(uniqid());
                 $objectManager->get('\Magento\Catalog\Api\CategoryRepositoryInterface')->save($category);
             }
             $id = $category->getId();
@@ -112,7 +112,7 @@ class CreateCategoriesApp extends AbstractApp
                 try {
                     $objectManager->get('\Magento\Catalog\Api\CategoryRepositoryInterface')->save($category);
                 } catch (Exception $e) {
-                    $category->setUrl($category->getUrl() + (string)uniqid());
+                    $category->setUrl(uniqid());
                     $objectManager->get('\Magento\Catalog\Api\CategoryRepositoryInterface')->save($category);
                 }
             }
