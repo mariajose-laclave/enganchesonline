0ea085934a0173b3761f443e3fd2425ce2035425
Grant MacDonald <phisoluciones.es@gmail.com>
2021-03-31 13:50:11 +0200

    Updte stock status.

--- scripts/create-products.php
+++ scripts/create-products.php
@@ -79,16 +79,20 @@ class CreateCategoriesApp extends AbstractApp
             $product->setStockData(
                 array(
                     'use_config_manage_stock' => 0,
-                    'manage_stock' => 0,
+                    'manage_stock' => 1,
                     'is_in_stock' => 1
                 )
             );
             $product->save();
             $categoryId = $objectManager->get('\Magento\Catalog\Model\CategoryFactory')
                 ->create()->getCollection()->addAttributeToFilter('name', $_product['product']->make)->getFirstItem()->getId();
-            $modelId = $objectManager->get('\Magento\Catalog\Model\CategoryFactory')
-                ->create()->getCollection()->addAttributeToFilter('name', $_product['product']->model)->getFirstItem()->getId();
-            $this->getCategoryLinkManagement()->assignProductToCategories($product->getSku(), [$categoryId, $modelId]);
+            $this->getCategoryLinkManagement()->assignProductToCategories($product->getSku(), [$categoryId]);
+                
+
+            $categories = $objectManager->get('\Magento\Catalog\Model\ResourceModel\Category\CollectionFactory')->create();
+            $make = $categories->addAttributeToFilter('name', array('eq' => strtolower($_product['product']->make)))->getFirstItem();
+            $model = $categories->addAttributeToFilter('name', array('eq' => strtolower($_product['product']->model)))->getFirstItem();
+            $this->getCategoryLinkManagement()->assignProductToCategories($product->getSku(), [$make->getId(), $model->getId()]);
         }
     }
     
