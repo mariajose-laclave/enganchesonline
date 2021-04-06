74108972fe8bb2b5a65cbbf448c30d82885cef99
Grant MacDonald <phisoluciones.es@gmail.com>
2021-04-06 11:30:15 +0200

    Include script for git

---
+++ scripts/pull.sh
@@ -0,0 +1,9 @@
+cd /var/www/vhosts/epic-dhawan.82-223-50-168.plesk.page/httpdocs
+git pull origin develop_
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy -f
+php bin/magento cache:clean
+php bin/magento cache:flush
+chown -R www-data ./
+chmod -R 777 ./
