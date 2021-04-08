495bae852247cc41642abe1c35297c6e08bee582
Grant MacDonald <phisoluciones.es@gmail.com>
2021-04-08 12:54:12 +0200

    Remove unused files

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/app/bootstrap.php';
-$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
-/** @var \Magento\Framework\App\Http $app */
-$app = $bootstrap->createApplication('TestApp');
-$bootstrap->run($app);
