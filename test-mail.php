2f0bc6d410af8cdae83da3dca0d1d2a0aafc6190
Grant MacDonald <phisoluciones.es@gmail.com>
2021-04-13 09:23:01 +0200

    Add PHP mailer to test

--- test-mail.php
+++ test-mail.php
@@ -2,25 +2,33 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-// the message
-$msg = "First line of text\nSecond line of text";
 
-// use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-// send email
-$headers = array("From: from@example.com",
-    "Reply-To: replyto@example.com",
-    "X-Mailer: PHP/" . PHP_VERSION
-);
-$to = 'grant@laclave.es';
-$message = 'test';
-$subject = 'test';
-$headers = implode("\r\n", $headers);
-$mail = mail($to, $subject, $message, $headers);
+try {
+    
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('phisoluciones.es@gmail.com', 'Joe User');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
 
-if (!$mail) {
-    echo 'Uuups...';   
-} else {
-    echo 'Mail sent';
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
