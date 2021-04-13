6a0a3c2b449c8c80197f41a8195bf2d5aa331c24
Grant MacDonald <phisoluciones.es@gmail.com>
2021-04-13 09:17:53 +0200

    Update email test file

--- test-mail.php
+++ test-mail.php
@@ -2,7 +2,6 @@
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-
 // the message
 $msg = "First line of text\nSecond line of text";
 
@@ -10,8 +9,18 @@ $msg = "First line of text\nSecond line of text";
 $msg = wordwrap($msg,70);
 
 // send email
+$headers = array("From: from@example.com",
+    "Reply-To: replyto@example.com",
+    "X-Mailer: PHP/" . PHP_VERSION
+);
+$to = 'grant@laclave.es';
+$message = 'test';
+$subject = 'test';
+$headers = implode("\r\n", $headers);
+$mail = mail($to, $subject, $message, $headers);
 
-
-if (!mail("phisoluciones.es@gmail.com","My subject",$msg)) {
+if (!$mail) {
     echo 'Uuups...';   
+} else {
+    echo 'Mail sent';
 }
