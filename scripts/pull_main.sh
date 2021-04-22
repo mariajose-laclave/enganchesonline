44b8a5c3ad4d063ff6e7452e27c2e8a237bed358
Grant MacDonald <phisoluciones.es@gmail.com>
2021-04-22 14:34:37 +0200

    Add main branch pull script

---
+++ scripts/pull_main.sh
@@ -0,0 +1,12 @@
+cd /var/www/vhosts/epic-dhawan.82-223-50-168.plesk.page/httpdocs
+git stash
+git checkout main
+git pull origin main
+php bin/magento cache:clean
+php bin/magento cache:flush
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy es_ES -f
+php bin/magento s:s:d -f
+chown -R www-data ./
+chmod -R 777 ./
